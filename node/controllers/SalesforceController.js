8558cc554e698d33ebc56003fc5b94814d44f654
Julian Marino <julycm92@gmail.com>
2023-05-29 02:50:14 -0300

    ultimos cambios

--- node/controllers/SalesforceController.js
+++ node/controllers/SalesforceController.js
@@ -1,6 +1,7 @@
 const fetch = require("node-fetch"); // Asegúrate de requerir cualquier módulo necesario
 const { PAGE_ACCESS_TOKEN,URL_CHAT,ORG_ID,DEPLOYMENT_ID,BUTTON_ID,USER_AGENT,LANGUAGE,SCREEN_RESOLUTION,CREATE_SESSION,CREATE_VISITOR_SESSION,CHAT_MESSAGE,MESSAGES,PAGE_ID,mappingSesion} = require('../global/Variables');
 const { log } = require("console");
+const request = require('request');
 
 
 /*
@@ -106,7 +107,8 @@ async function createSFSession(senderID) {
         headers: {
           "X-LIVEAGENT-API-VERSION": 34,
           "X-LIVEAGENT-AFFINITY": session.affinityToken,
-          "X-LIVEAGENT-SESSION-KEY": session.sessionKey
+          "X-LIVEAGENT-SESSION-KEY": session.sessionKey,
+          'Content-Type': 'application/json'
         },
         body: JSON.stringify(data)
       });
@@ -116,6 +118,7 @@ async function createSFSession(senderID) {
         console.log("Mensaje enviado correctamente, body: %s", body);
       } else {
         console.log(response);
+        console.log(JSON.stringify(response));
         console.error("Failed calling chatMessage", response.status, response.statusText);
       }
     } catch (error) {
@@ -147,7 +150,7 @@ async function getSFMessages(senderID) {
         body.messages.forEach(async function(message) {
             switch (message.type) {
                 case 'ChatMessage':
-                    console.log("Message recibido: %s", message);
+                    console.log("Message recibido: %s", JSON.stringify(message));
                     await sendIGMessage(message,senderID);
                     getSFMessages(senderID);
                     break;
@@ -171,33 +174,86 @@ async function getSFMessages(senderID) {
 /*
  * Create a Salesforce Chat Session. If successful, we'll 
  * get the session metadata 
- *
  */
+
 async function sendIGMessage(message, senderID) {
-    var data = {
-        message: {"text": message.text}
-    };
-    
-    try {
-      const response = await fetch(`https://graph.facebook.com/v16.0/${PAGE_ID}`, {
-        method: 'POST',
-        qs: {
-            recipient: {id: senderID},
-            access_token: PAGE_ACCESS_TOKEN
-        },
-        body: JSON.stringify(data)
-      });
-  
+  var data = {
+      recipient: {
+          id: senderID
+      },
+      message: {"text": message.message.text}
+  };
+
+  var url = `https://graph.facebook.com/v16.0/${PAGE_ID}/messages?access_token=${PAGE_ACCESS_TOKEN}`;
+
+  var options = {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+  };
+
+  console.log("armado URL sendIGmessage: %s", JSON.stringify(options));
+
+  try {
+      const response = await fetch(url, options);
       if (response.ok) {
-        const body = await response.json();
-        console.log("Mensaje enviado exitosamente, body: %s", body);
+          const body = await response.json();
+          console.log("Mensaje enviado exitosamente, body: %s", JSON.stringify(body));
       } else {
-        console.log(response);
-        console.error("Failed calling sendIGMessage", response.status, response.statusText);
+          console.log(response);
+          console.error("Failed calling sendIGMessage", response.status, response.statusText);
       }
-    } catch (error) {
+  } catch (error) {
       console.error("Error:", error);
-    }
+  }
 }
   
+
+/* USANDO EJEMPLO DE ME
+async function sendIGMessage(message, senderID) {
+  const data = {
+      recipient: {
+          id: senderID
+      },
+      message: {
+          text: message.message.text,
+          metadata: "DEVELOPER_DEFINED_METADATA"
+      }
+  };
+
+  const url = `https://graph.facebook.com/v2.6/me/messages?access_token=${PAGE_ACCESS_TOKEN}`;
+
+  try {
+      const response = await fetch(url, {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data)
+      });
+
+      if (!response.ok) {
+          console.error("Failed calling Send API", response.status, response.statusText);
+          const errorBody = await response.json();
+          console.error(errorBody.error);
+          return;
+      }
+
+      const responseBody = await response.json();
+      const recipientId = responseBody.recipient_id;
+      const messageId = responseBody.message_id;
+
+      if (messageId) {
+          console.log("Successfully sent message with id %s to recipient %s", messageId, recipientId);
+      } else {
+          console.log("Successfully called Send API for recipient %s", recipientId);
+      }
+  } catch (error) {
+      console.error("Error:", error);
+  }
+}
+*/
+
 module.exports = { createSFSession,createSFVisitorSession,chatMessage,getSFMessages };
